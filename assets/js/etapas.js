2f97e8f277af225d4a50c83b7753df3e9c42fcad
CamilodeAssis <camilo.kotecki10@gmail.com>
2022-08-10 15:52:02 -0300

    Atualizando funções

--- assets/js/etapas.js
+++ assets/js/etapas.js
@@ -4,19 +4,19 @@ let etapas = [
         numeros: 5,
         candidatos: [
             {
-                numero: 38111,
+                numero: "38111",
                 nome: 'Fulano de Tal',
                 partido: 'ABC',
                 fotos: [
-                    { url: 'assets/images/gui.jpg', legenda: 'Vereador', small: false }
+                    { url: 'gui.jpg', legenda: 'Vereador', small: false }
                 ]
             },
             {
-                numero: 77222,
+                numero: "77222",
                 nome: 'Beltrano da Silva',
                 partido: 'DEFG',
                 fotos: [
-                    { url: 'assets/images/zoio.jpg', legenda: 'Vereador', small: false }
+                    { url: 'zoio.jpg', legenda: 'Vereador', small: false }
                 ]
             }
         ]
@@ -26,23 +26,23 @@ let etapas = [
         numeros: 2,
         candidatos: [
             {
-                numero: 99,
+                numero: "99",
                 nome: 'Ciclano',
                 partido: 'ABC',
                 vice: 'Cic',
                 fotos: [
-                    { url: 'assets/images/tikinho.jpg', legenda: 'Prefeito', small: false },
-                    { url: 'assets/images/kg.jpg', legenda: 'Vice-Prefeito', small: true }
+                    { url: 'tikinho.jpg', legenda: 'Prefeito', small: false },
+                    { url: 'kg.jpg', legenda: 'Vice-Prefeito', small: true }
                 ]
             },
             {
-                numero: 84,
+                numero: "84",
                 nome: 'Zulano',
                 partido: 'QWERTY',
                 vice: 'Zul',
                 fotos: [
-                    { url: 'assets/images/tikinho.jpg', legenda: 'Prefeito', small: false },
-                    { url: 'assets/images/kg.jpg', legenda: 'Vice-Prefeito', small: true }
+                    { url: 'tikinho.jpg', legenda: 'Prefeito', small: false },
+                    { url: 'kg.jpg', legenda: 'Vice-Prefeito', small: true }
                 ]
             }
         ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,65 @@ let cs = (el) => document.querySelectorAll(el);
 
 let seuVotoPara = c('.d1-1 span');
 let cargo = c('.d1-2 span');
-let descriçao = c('.d1-4');
+let descricao = c('.d1-4');
 let aviso = c('.d-2');
 let lateral = c('.d1-right');
 let numeros = c('.d1-3');
 
 let etapaAtual = 0;
+let numero = '';
+let branco = false;
 
-
-function começarEtapa() {
-    let etapa =etapas[etapaAtual];
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
 
     let numeroHTML = "";
+     numero = '';
+     branco = false;
+
+    for (let i = 0; i < etapa.numeros; i++) {
+        if (i === 0) {
+            numeroHTML += '<div class="numero foco"></div>';
+        } else {
+            numeroHTML += '<div class="numero"></div>';
+        }
+
+    }
 
     seuVotoPara.style.display = 'none';
     cargo.innerHTML = etapa.titulo;
-    descriçao.innerHTML = "";
+    descricao.innerHTML = "";
     aviso.style.display = 'none';
     lateral.innerHTML = "";
     numeros.innerHTML = numeroHTML;
 }
 
+function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item) => {
+        if (item.numero === numero) {
+            return true;
+        } else
+            return false;
+    });
+
+    if (candidato.length > 0) {
+        candidato = candidato[0]
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+
+        let fotosHtml = '';
+        for (let i in candidato.fotos){
+            fotosHtml += `<div class="d1-img"><img src="/assets/images/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+        }
+        lateral.innerHTML = fotosHtml;
+    } else {
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="avisoNuloBranco foco">VOTO NULO!<div/>'
+    }
+}
 
 
 
@@ -31,19 +69,39 @@ let botao = cs('[data-number]');
 
 botao.forEach(b => {
     b.addEventListener('click', () => {
-        const number = b.dataset.number
-        alert(`Você clicou ${number}`);
-    })
+        const n = b.dataset.number
+        let numeroEl = c('.numero.foco');
+        if (numeroEl !== null) {
+            numeroEl.innerHTML = n;
+            numero = `${numero}${n}`;
+
+            numeroEl.classList.remove('foco');
+            if (numeroEl.nextElementSibling !== null) {
+                numeroEl.nextElementSibling.classList.add('foco');
+            } else {
+                atualizaInterface();
+            }
+        }
+    });
+
 });
 
-let branco = c('.branco').addEventListener('click', () => {
-    alert(`Você clicou BRANCO`);
+let botaoBranco = c('.branco').addEventListener('click', () => {
+    if (numero === ''){
+        branco = true;
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        numeros.innerHTML = '';
+        descricao.innerHTML = '<div class="avisoNuloBranco foco">VOTO EM BRANCO!<div/>'
+    } else {
+        alert('PARA VOTAR EM BRANCO NÃO PODE TER DIGITADO NENHUM NUMERO. APERTE CORRIGE PARA REINICIAR. ')
+    }
 });
-let corrige = c('.corrige').addEventListener('click', () => {
-    alert(`Você clicou CORRIGE`);
+let botaoCorrige = c('.corrige').addEventListener('click', () => {
+    comecarEtapa();
 });
-let confirma = c('.confirma').addEventListener('click', () => {
+let botaoConfirma = c('.confirma').addEventListener('click', () => {
     alert(`Você clicou CONFIRMA`);
 });
 
-começarEtapa();
+comecarEtapa();
